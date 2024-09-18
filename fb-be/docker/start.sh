e65fd314d083b463b2188dd8e303076281e57170
Brunino Criniti <Brunino.Criniti@konicaminolta.it>
2024-09-18 16:48:24 +0200

    Filebot docker installation

--- fb-be/docker/start.sh
+++ fb-be/docker/start.sh
@@ -4,13 +4,11 @@
 set -e   # Exit immediately if any command fails
 set -u   # Treat unset variables as an error
 set -x   # Enable debugging mode to print commands being executed
-
 # Ensure the filebot binary is available
 if ! command -v filebot > /dev/null 2>&1; then
     echo "FileBot is not installed or not found in PATH"
     exit 1
 fi
-
 # Check if FileBot license file exists and copy it to the correct location
 if [ -f "$FILEBOT_LICENSE_PATH" ]; then
     echo "Copying FileBot license file from $FILEBOT_LICENSE_PATH to /opt/filebot/.license/license.psm"
@@ -26,6 +24,5 @@ if [ -f "$FILEBOT_LICENSE_PATH" ]; then
 else
     echo "FileBot license file not found at $FILEBOT_LICENSE_PATH, continuing without applying license."
 fi
-
 # Run the CMD passed from Dockerfile
 exec "$@"
