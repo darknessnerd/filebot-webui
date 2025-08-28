dae324403323a57f7de0c48f6e51f287975f5a36
br1 <crinitib@email.com>
2025-08-28 20:32:35 +0200

    feat: rework

--- cmd/webui-be/main.go
+++ cmd/webui-be/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"os"
 
 	"webui-skeleton/internal/app"
 	"webui-skeleton/internal/logger"
@@ -12,6 +13,12 @@ var TemplateFS embed.FS
 
 func main() {
 	logger.Log.Info().Msg("Starting application...")
+	// Log DIRECTORY_PRESETS value for debugging
+	directoryPresets := os.Getenv("DIRECTORY_PRESETS")
+	logger.Log.Info().Msgf("DIRECTORY_PRESETS value: %s", directoryPresets)
+	if directoryPresets == "" {
+		logger.Log.Fatal().Msg("DIRECTORY_PRESETS environment variable must be set. Example: DIRECTORY_PRESETS=Downloads:/downloads,Media Library:/media")
+	}
 	// Create and initialize application
 	application := app.New(TemplateFS)
 	defer application.Cleanup()
