72cf7b3686fe7dc41b0fb709c724158bfd1774a9
br1 <crinitib@email.com>
2025-08-28 19:54:37 +0200

    feat: rework

--- cmd/webui-be/main.go
+++ cmd/webui-be/main.go
@@ -12,6 +12,11 @@ var TemplateFS embed.FS
 
 func main() {
 	logger.Log.Info().Msg("Starting application...")
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Log.Error().Msgf("❌ Application panicked: %v", r)
+		}
+	}()
 	// Create and initialize application
 	application := app.New(TemplateFS)
 	defer application.Cleanup()
@@ -23,6 +28,7 @@ func main() {
 
 	// Run the application
 	if err := application.Run(); err != nil {
-		logger.Log.Fatal().Err(err).Msg("❌ Application failed to run")
+		logger.Log.Error().Err(err).Msg("❌ Application failed to run")
+		logger.Log.Error().Msgf("❌ Application exited with code: 1")
 	}
 }
